f39dc83edccac72182a9f4460332d67ec4c071e5
WuX <wux@thenest.dev>
2025-05-02 20:24:16 +0300

    config: group home manager configuration together

---
+++ config/home-manager/default.nix
@@ -0,0 +1,13 @@
+{ ... }: {
+    imports = [
+        ./hypr.nix
+
+        ./programs/zed.nix
+        ./programs/git.nix
+        ./programs/vscode.nix
+        ./programs/bash.nix
+        ./programs/alacritty.nix
+        ./programs/bemenu.nix
+        ./programs/neovim.nix
+    ];
+}
