d8886164bf4da772437f4bcb792916ef89ea327a
WuX <wux@thenest.dev>
2025-05-02 22:20:24 +0300

    mksystem: tie it all together

---
+++ mksystem.nix
@@ -0,0 +1,37 @@
+{ inputs, outputs }:
+{ id, name, system }:
+inputs.nixpkgs.lib.nixosSystem {
+    inherit system;
+
+    specialArgs = { inherit inputs outputs; inherit system; };
+
+    modules = [
+        inputs.home-manager.nixosModules.home-manager
+
+        inputs.nixcfg-private.config.private
+
+        ./config/common.nix
+        ./config/machines/${id}
+
+        {
+            imports = builtins.attrValues outputs.nixosModules;
+
+            nixpkgs.overlays = builtins.attrValues outputs.overlays;
+
+            networking.hostName = name;
+
+            home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+
+                extraSpecialArgs = { inherit inputs outputs; inherit system; };
+
+                users.wux = {
+                    programs.home-manager.enable = true;
+                    imports = [ ./config/home-manager ];
+                    home.stateVersion = "24.11";
+                };
+            };
+        }
+    ];
+}
