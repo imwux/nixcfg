39f8b0aeba7ee7adc13e649162ffe96b868df82c
WuX <wux@thenest.dev>
2025-05-02 22:19:48 +0300

    config: add common configuration

---
+++ config/common-graphical.nix
@@ -0,0 +1,72 @@
+{ config, pkgs, ... }: {
+    imports = [
+        ./nixos/firefox.nix
+    ];
+
+    users.users.wux.extraGroups = [ "video" "audio" ];
+
+    unfree = [ "nvidia-x11" "nvidia-settings" ];
+
+    console = {
+        font = "${pkgs.terminus_font}/share/consolefonts/ter-132n.psf.gz";
+        packages = with pkgs; [ terminus_font ];
+        keyMap = "fi";
+    };
+
+    fonts.packages = with pkgs; [ dejavu_fonts ];
+
+    programs.hyprland.enable = true;
+    programs.hyprlock.enable = true;
+    environment.sessionVariables = {
+        NIXOS_OZONE_WL = 1;
+        WLR_NO_HARDWARE_CURSORS = 1;
+        NIX_SHELL_PRESERVE_PROMPT = 1;
+    };
+
+    qt = {
+        enable = true;
+        style = "adwaita-dark";
+        platformTheme = "gnome";
+    };
+
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        jack.enable = true;
+    };
+    nixpkgs.config.pulseaudio = true;
+
+    services.gvfs.enable = true; # Needed for Astal (UI)
+
+    programs.firefox.enable = true;
+
+    home-manager.users.wux = {
+        wayland.windowManager.hyprland.enable = true;
+        programs.hyprlock.enable = true;
+        systemd.user.sessionVariables = config.home-manager.users.wux.home.sessionVariables;
+        services.hyprpaper.enable = true;
+        services.mpris-proxy.enable = true;
+
+        home.pointerCursor = {
+            name = "phinger-cursors-dark";
+            package = pkgs.phinger-cursors;
+            size = 32;
+            gtk.enable = true;
+        };
+
+        gtk = {
+            enable = true;
+            theme = {
+                name = "adw-gtk3-dark";
+                package = pkgs.adw-gtk3;
+            };
+        };
+
+        programs.bemenu.enable = true;
+        programs.alacritty.enable = true;
+        programs.vscode.enable = true;
+        programs.zed-editor.enable = true;
+    };
+}
---
+++ config/common.nix
@@ -0,0 +1,59 @@
+{ ... }: {
+    imports = [
+        ./nixos/unfree.nix
+    ];
+
+    nix = {
+        settings = {
+            experimental-features = [ "nix-command" "flakes" ];
+            trusted-users = [ "root" "wux" ];
+        };
+
+        gc = {
+            automatic = true;
+            persistent = true;
+            dates = "48hr";
+            randomizedDelaySec = "10min";
+        };
+    };
+
+    users.users.wux = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" ];
+    };
+
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    security = {
+        sudo.wheelNeedsPassword = false;
+        polkit.enable = true;
+        rtkit.enable = true;
+
+        pam.loginLimits = [
+            { domain = "@wheel"; item = "nofile"; type = "soft"; value = "524288"; }
+            { domain = "@wheel"; item = "nofile"; type = "hard"; value = "1048576"; }
+        ];
+    };
+
+    programs.nano.enable = false;
+
+    programs.git = {
+        enable = true;
+        lfs.enable = true;
+    };
+
+    environment.variables = {
+        GOPATH = "$HOME/.config/go";
+        GOMODCACHE = "$HOME/.cache/go/mod";
+    };
+
+    home-manager.users.wux = {
+        programs.bash.enable = true;
+        programs.git.enable = true;
+        programs.neovim.enable = true;
+
+        xdg.configFile."wgetrc".text = "hsts_file = \"~/.cache/wget-hsts\"";
+    };
+
+    system.stateVersion = "24.11";
+}
