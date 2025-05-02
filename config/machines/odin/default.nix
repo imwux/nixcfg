a8f284ea26b6241b7449b9677d8cd55232c092d9
WuX <wux@thenest.dev>
2025-05-02 22:20:05 +0300

    config: add Odin machine

---
+++ config/machines/odin/default.nix
@@ -0,0 +1,65 @@
+{ inputs, system, pkgs, ... }: {
+    imports = [
+        ./hardware.nix
+        ../../common-graphical.nix
+    ];
+
+    time.timeZone = "Europe/Helsinki";
+
+    networking.hostName = "Odin";
+
+    networking.networkmanager.enable = true;
+    users.users.wux.extraGroups = [ "networkmanager" ];
+
+    unfree = [ "vscode" "discord" "spotify" "vscode-extension-ms-vscode-cpptools" ];
+
+    services.xserver.videoDrivers = [ "nvidia" ];
+
+    services.upower.enable = true;
+
+    programs.light.enable = true;
+
+    services.udev.extraRules = ''
+        ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="intel_backlight", RUN+="${pkgs.coreutils-full}/bin/chgrp video /sys/class/backlight/%k/brightness"
+        ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="intel_backlight", RUN+="${pkgs.coreutils-full}/bin/chmod g+w /sys/class/backlight/%k/brightness"
+    '';
+
+    services.playerctld.enable = true;
+
+    home-manager.users.wux = {
+        home.packages = with pkgs; [
+            spotify
+            discord
+            imagemagick
+            gcc14
+        ];
+
+        wayland.windowManager.hyprland.settings = {
+            exec-once = [ "${inputs.wuxs-ui.packages.${system}.default}/bin/wuxs-ui" ];
+
+            monitor = [ ",preferred,auto,2" ];
+
+            input.kb_layout = "fi";
+
+            animations.enabled = false;
+
+            decoration = {
+                shadow.enabled = false;
+                blur.enabled = false;
+                rounding = 0;
+            };
+
+            general = {
+                "gaps_out" = 0;
+                "gaps_in" = 0;
+            };
+        };
+
+        services.hyprpaper.settings = let wp_1 = toString pkgs.wallpaper.landscape-cloudy-mountain; in {
+            preload = [wp_1];
+            wallpaper = [
+                "eDP-1,${wp_1}"
+            ];
+        };
+    };
+}
---
+++ config/machines/odin/hardware.nix
@@ -0,0 +1,86 @@
+{ lib, config, pkgs, ... }: {
+    boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+    boot.initrd.kernelModules = [ "i915" ];
+    boot.kernelModules = [ "kvm-intel" ];
+    boot.extraModulePackages = [];
+
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    boot.kernelParams = [ "i915.enable_guc=2" ];
+
+    fileSystems = {
+        "/" = {
+            device = "/dev/disk/by-uuid/112835ba-9664-4753-862d-b7421aa1c5d6";
+            fsType = "ext4";
+        };
+        "/boot" = {
+            device = "/dev/disk/by-uuid/D14F-3F94";
+            fsType = "vfat";
+            options = [ "fmask=0077" "dmask=0077" ];
+        };
+    };
+
+    swapDevices = [{ device = "/dev/disk/by-uuid/06d373d0-d269-4db6-80b3-56c23c1849ae"; }];
+
+    networking.useDHCP = lib.mkDefault true;
+
+    nixpkgs.hostPlatform = "x86_64-linux";
+
+    services.fstrim.enable = true;
+
+    services.thermald.enable = true;
+
+    services.auto-cpufreq = {
+        enable = true;
+        settings = {
+            battery = {
+                governor = "powersave";
+                turbo = "never";
+            };
+            charger = {
+                governor = "performance";
+                turbo = "auto";
+            };
+        };
+    };
+
+    hardware = {
+        enableRedistributableFirmware = true;
+
+        bluetooth.enable = true;
+
+        graphics = {
+            enable = true;
+            enable32Bit = true;
+            extraPackages = with pkgs; [
+                intel-media-driver
+                intel-compute-runtime
+                vpl-gpu-rt
+            ];
+            extraPackages32 = with pkgs.driversi686Linux; [ intel-media-driver ];
+        };
+
+        nvidia = {
+            open = false;
+            modesetting.enable = true;
+            powerManagement = {
+                enable = true;
+                finegrained = true;
+            };
+            nvidiaSettings = true;
+            package = config.boot.kernelPackages.nvidiaPackages.production;
+
+            prime = {
+                offload = {
+                    enable = true;
+                    enableOffloadCmd = true;
+                };
+                intelBusId = "PCI:0:2:0";
+                nvidiaBusId = "PCI:1:0:0";
+            };
+        };
+
+        cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
+    };
+}
