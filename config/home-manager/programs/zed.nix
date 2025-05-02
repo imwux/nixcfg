4d3805537a5ce271e58a49cff70742bd060697a7
WuX <wux@thenest.dev>
2025-05-02 17:05:09 +0300

    config: add zed-editor configuration

---
+++ config/home-manager/programs/zed.nix
@@ -0,0 +1,35 @@
+{ pkgs, ... }:
+{
+    programs.zed-editor = {
+        package = pkgs.unstable.zed-editor;
+        extensions = [ "nix" "make" "vscode_dark_modern" "toml" "scheme" "assembly" "meson"];
+        userSettings = {
+            theme = {
+                mode = "dark";
+                dark = "VSCode Dark Modern";
+                light = "One Light";
+            };
+
+            tab_size = 4;
+            buffer_font_size = 15;
+            buffer_font_family = "RandyGG";
+
+            format_on_save = "on";
+
+            terminal.cursor_shape = "bar";
+
+            edit_predictions.mode = "subtle";
+
+            languages = {
+                Meson.enable_language_server = false;
+                Nix.language_servers = [ "nil" ];
+            };
+
+            lsp.clangd.binary.path = "${pkgs.llvmPackages_19.clang-tools}/bin/clangd";
+            lsp.rust-analyzer.binary.path = "${pkgs.rust-analyzer}/bin/rust-analyzer";
+            lsp.lua-language-server.binary.path = "${pkgs.lua-language-server}/bin/lua-language-server";
+            lsp.pylsp.binary.path = "${pkgs.python312Packages.python-lsp-server}/bin/pylsp";
+            lsp.nil.binary.path = "${pkgs.nil}/bin/nil";
+        };
+    };
+}
