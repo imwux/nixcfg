fdb42c1f6f6ca514570e0a53fe7322392b52a127
WuX <wux@thenest.dev>
2025-05-02 17:04:56 +0300

    config: add vscode configuration

---
+++ config/home-manager/programs/vscode.nix
@@ -0,0 +1,71 @@
+{ inputs, pkgs, ... }:
+{
+    programs.vscode = {
+        userSettings = {
+            "update.mode" = "none";
+
+            "window.commandCenter" = true;
+            "window.menuBarVisibility" = "classic";
+
+            "workbench.colorTheme" = "Default Dark Modern";
+            "workbench.startupEditor" = "none";
+            "workbench.activityBar.location" = "top";
+
+            "editor.renderWhitespace" = "trailing";
+            "editor.cursorSmoothCaretAnimation" = "on";
+            "editor.cursorBlinking" = "phase";
+            "editor.bracketPairColorization.enabled" = false;
+            "editor.quickSuggestionsDelay" = 0;
+            "editor.minimap.renderCharacters" = false;
+            "editor.fontFamily" = "RandyGG, Consolas, monospace";
+            "editor.fontSize" = 15;
+            "editor.selectionClipboard" = false;
+            "editor.emptySelectionClipboard" = false;
+
+            "terminal.integrated.cursorStyle" = "line";
+            "files.autoSave" = "off";
+            "files.associations" = { "*.h" = "c"; };
+            "extensions.ignoreRecommendations" = true;
+            "remote.autoForwardPorts" = false;
+
+            "git.confirmSync" = false;
+
+            "todo-tree.regex.regex" = "(//|/\\*|\\s\\*|#|<!--|;|^|^[ \\t]*(-|\\d+.))\\s*($TAGS)";
+            "todo-tree.general.tags" = [
+                "TODO"      "@todo"
+                "BUG"       "@bug"
+                "CRITICAL"  "@critical"
+                "OPTIMIZE"  "@optimize"
+            ];
+            "todo-tree.highlights.backgroundColourScheme" = [
+                "yellow"    "yellow"
+                "orange"    "orange"
+                "red"       "red"
+                "green"     "green"
+            ];
+            "todo-tree.highlights.foregroundColourScheme" = [
+                "black" "black"
+                "black" "black"
+                "white" "white"
+                "black" "black"
+            ];
+            "todo-tree.general.revealBehaviour" = "start of todo";
+            "todo-tree.highlights.highlightDelay" = 0;
+            "todo-tree.highlights.useColourScheme" = true;
+
+            "C_Cpp.clang_format_path" = "${pkgs.llvmPackages_19.clang-tools}/bin/clang-format";
+
+            "asm-code-lens.enableCodeLenses" = false;
+        };
+        extensions = with pkgs.vscode-extensions; [
+            bbenoist.nix
+            gruntfuggly.todo-tree
+            ms-vscode.cpptools
+            inputs.nix-vscode-extensions.extensions.x86_64-linux.vscode-marketplace.ms-vscode.cpptools-themes
+            golang.go
+            tamasfe.even-better-toml
+            inputs.nix-vscode-extensions.extensions.x86_64-linux.vscode-marketplace.basdp.language-gas-x86
+            inputs.nix-vscode-extensions.extensions.x86_64-linux.vscode-marketplace.maziac.asm-code-lens
+        ];
+    };
+}
