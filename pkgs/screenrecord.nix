0fee46e2414830cd50d51cbe774e1cc14daf374b
WuX <wux@thenest.dev>
2025-05-05 17:55:00 +0300

    config: remove the red border from slurp

--- pkgs/screenrecord.nix
+++ pkgs/screenrecord.nix
@@ -1,4 +1,4 @@
 { pkgs, writeShellScriptBin, ... }:
 writeShellScriptBin "screenrecord" ''
-    ${pkgs.wf-recorder}/bin/wf-recorder -g "$(${pkgs.slurp}/bin/slurp -c '#ff0000ff')" -m mp4 --file="''${1:-recording.mp4}"
+    ${pkgs.wf-recorder}/bin/wf-recorder -g "$(${pkgs.slurp}/bin/slurp)" -m mp4 --file="''${1:-recording.mp4}"
 ''
